d91446b925c566037ed036e88c8aaea1513c3106
mustafaozvardar <mustafaozvardar77@gmail.com>
2024-08-12 12:49:56 +0200

    first commit

---
+++ security_camera.py
@@ -0,0 +1,24 @@
+import cv2
+import winsound
+import time
+
+webcam= cv2.VideoCapture(0)
+while True:
+    _,im1 = webcam.read()
+    _,im2 = webcam.read()
+    diff = cv2.absdiff(im1,im2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 20, 255, cv2.THRESH_BINARY)
+    contours,_ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        if cv2.contourArea(c) < 5000:
+            continue
+        for _ in range(3):
+            winsound.Beep(1000, 500)
+            time.sleep(0.001) 
+    cv2.imshow("Security Camera", thresh)
+    if cv2.waitKey(10) == 27:
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
